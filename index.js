b9e00376a1f4fd39770f0770db9b823d682148e4
dangphuc2611 <cuccukgiontan2611@gmail.com>
2026-05-26 16:36:33 +0700

    refactor: remove server boilerplate and add verbose logging to webhook processing logic

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-const bodyParser = require("body-parser");
-
-const app = express();
-
-app.use(bodyParser.json());
-
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const FLOWISE_API = process.env.FLOWISE_API;
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("WEBHOOK VERIFIED");
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
 app.post("/webhook", async (req, res) => {
+  console.log("WEBHOOK HIT");
+  console.log(JSON.stringify(req.body, null, 2));
+
   try {
     const entry = req.body.entry?.[0];
     const messaging = entry?.messaging?.[0];
@@ -36,7 +9,12 @@ app.post("/webhook", async (req, res) => {
     const senderId = messaging?.sender?.id;
     const message = messaging?.message?.text;
 
+    console.log("Sender ID:", senderId);
+    console.log("Message:", message);
+
     if (message) {
+      console.log("Sending to Flowise...");
+
       // Send to Flowise
       const aiResponse = await axios.post(
         FLOWISE_API,
@@ -45,11 +23,17 @@ app.post("/webhook", async (req, res) => {
         }
       );
 
+      console.log("Flowise response:", aiResponse.data);
+
       const reply =
         aiResponse.data.text || "Sorry, no response.";
 
+      console.log("Reply:", reply);
+
+      console.log("Sending reply to Facebook...");
+
       // Reply to Facebook
-      await axios.post(
+      const fbResponse = await axios.post(
         `https://graph.facebook.com/v19.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
         {
           recipient: {
@@ -60,17 +44,16 @@ app.post("/webhook", async (req, res) => {
           }
         }
       );
+
+      console.log("Facebook response:", fbResponse.data);
     }
 
     res.sendStatus(200);
+
   } catch (err) {
-    console.error(err.message);
+    console.error("ERROR:");
+    console.error(err.response?.data || err.message);
+
     res.sendStatus(500);
   }
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
 });
